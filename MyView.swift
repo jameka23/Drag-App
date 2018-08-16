873807d9d7f43946c88f40ed6847ea3c4669f0f3
Jameka Echols <jameka.echols@gmail.com>
2018-08-15 22:17:59 -0500

    first commit of Drag App

---
+++ MyView.swift
@@ -0,0 +1,23 @@
+//
+//  MyView.swift
+//  Drag
+//
+//  Created by Jameka Echols on 8/15/18.
+//  Copyright © 2018 Jameka Echols. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    var lastLocation = CGPoint(x: 0, y: 0)
+    
+
+}
